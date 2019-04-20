1c23adbbaf2e358256c48f140d15f3de4a21822c
tomgdow <thomasgdowling@gmail.com>
2019-04-20 20:13:38 +0100

    update data/update_data.js to include bash scripting

--- data/update_data.js
+++ data/update_data.js
@@ -5,6 +5,7 @@ const remoteFileDirectory="~/bin/jsdefinitions/operator-precedence-rememberme";
 const remoteFileName=remoteFileDirectory.split('/').pop();
 
 let my_update = `
+// FROM VISUAL STUDIO ON WINDOWS 10 
     £(1)  powershell                                 // from VS code terminal
     £(2)  ssh tomgxu
     £(3)  // on remote (xu virtual machine)
@@ -35,9 +36,17 @@ let my_update = `
 
 // Note Data
 const txtString=[
+  `BASH SCRIPTING`,
+  `(from xu virtual machine)`,
+  `(1) pre-update  // bash script`,
+  `(2) vim ~/acp/precedence_remember_me_spa/data/data.js`,
+  `(3) :r ~/operator-precedence-rememberme  //vim, from within backticks`,
+  `(4) pre-cleanup  // bash script !! careful`,
+  `(5) update git and push to github`,
+  `(6) sudo git pull from /var/www/html/precedence_remember_me`,
+  `(7) sudo service apache2 restart`,
   `NOTES`,
  `(1) The primary data are stored on the virtual machine 'xu',`,
  `(2) Directory:  '${remoteFileDirectory}'`,
- `(3) The file '${remoteFileName}' will be downloaded`,
- `(4) github repo (private): https://github.com/tomGdow/${repoName}`
+ `(3) github repo (private): https://github.com/tomGdow/${repoName}`
 ];
